d5912fbca530b7fa456100efb2963635c83f6f9f
floating-cat <monsoon.cl@gmail.com>
2025-02-07 19:53:20 +0000

    Use Nix to build the project

---
+++ nix/tree-sitter-devicetree.nix
@@ -0,0 +1,41 @@
+# forked from https://github.com/urob/zmk-config/blob/4d00a15b88a4647ff1d09515afac969567bfb306/nix/tree-sitter-devicetree.nix
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  tree-sitter,
+}:
+
+buildPythonPackage rec {
+  pname = "tree-sitter-devicetree";
+  version = "0.12.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "joelspadin";
+    repo = "tree-sitter-devicetree";
+    rev = "16c9cb959675bc9bc4f4e5bebe473d511a12a06d";
+    hash = "sha256-UVxLF4IKRXexz+PbSlypS/1QsWXkS/iYVbgmFCgjvZM=";
+  };
+
+  build-system = [
+    setuptools
+  ];
+
+  optional-dependencies = {
+    core = [
+      tree-sitter
+    ];
+  };
+
+  # There are no tests
+  doCheck = false;
+  pythonImportsCheck = [ "tree_sitter_devicetree" ];
+
+  meta = {
+    description = "Devicetree grammar for tree-sitter";
+    homepage = "https://github.com/joelspadin/tree-sitter-devicetree";
+    license = lib.licenses.mit;
+  };
+}
